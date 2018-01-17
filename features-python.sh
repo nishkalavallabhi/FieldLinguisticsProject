da1e4bbd197f3fba0409a3e9cb0e8a751e94785e
Sowmya <vbsowmya@gmail.com>
2018-01-17 16:01:23 -0600

    Update features-python.sh

--- features-python.sh
+++ features-python.sh
@@ -1,3 +1,4 @@
+#Run this from OpenSmile installation folder, giving the audio file (.wav format) as input
 ./SMILExtract -C config/IS13_ComParE.conf -I $1 -O trial_output_temp
 tail -1 trial_output_temp
 rm trial_output_temp
