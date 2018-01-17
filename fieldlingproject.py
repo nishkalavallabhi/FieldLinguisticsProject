c16ed675453e2bef7fc15f4b193fd5b840c1ff3c
Sowmya <vbsowmya@gmail.com>
2018-01-17 17:06:08 -0600

    added comments for @embedsri

--- fieldlingproject.py
+++ fieldlingproject.py
@@ -7,27 +7,32 @@ I am running this file from OpenSmile Installation folder.
 
 import sys,subprocess,os,math
 from scipy import spatial
-#script: features-python.sh, takes 1 argument. a wav file. 
 
 #Extracts opensmile features for all IPA sounds, and stores them in disk
+#This has to be run once, and the results are stored in the ipa-features file. 
 def store_features_IPA(dir_path):
    files = os.listdir(dir_path)
-   fw = open("ipa-features","w")
+   fw = open("ipa-features","w") #a file to store audio features for all IPA sounds in the folder.
    for f in files:
        fpath = os.path.join(dir_path,f)
+       #script features-python.sh takes 1 argument. a wav file. 
+       #subprocess execution returns bytes, so decode to utf8
        out = subprocess.check_output(['sh', 'features-python.sh',fpath]).decode("utf-8")
        temp = out.split(",")
+       #we need all columns in opensmile output except first and the last ones.
        to_write = f.split(".mp3")[0]+","+ ",".join(temp[1:len(temp)-2])
        fw.write(to_write)
        fw.write("\n")
        print("Wrote features for: ", f)
    fw.close()
 
+#get opensmile features for the input audio file.
 def get_features(file_path): #Full file path. 
     out = subprocess.check_output(['sh', 'features-python.sh',file_path]).decode("utf-8")
     temp = out.split(",")
     return [float(i) for i in temp[1:len(temp)-2]]
 
+#load ipa features file into program, and save it as two lists - one containing sound names, one with sound features
 def load_features_IPA(ipa_features_path):
     fh = open(ipa_features_path)
     sounds = []
@@ -38,6 +43,7 @@ def load_features_IPA(ipa_features_path):
        features.append([float(i) for i in what_we_need[1:]])
     return sounds,features
 
+#use KDTree to quickly calculate similarity and get the closest ipa sound to the input file.
 def get_similar_sounds(sound_feat,ipafeats,sounds):
     tree = spatial.KDTree(ipafeats) #use k=i to get i nearest neighbors instead of the closest one.
     return sounds[tree.query(sound_feat)[1]]
